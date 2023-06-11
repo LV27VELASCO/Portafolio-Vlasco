da03177ac189c16a2d039e12f5ae85bba8a21bcf
Luis Velasco <luiselsw27@gmail.com>
2023-06-10 19:18:13 -0500

    .

--- src/components/SobreMi.jsx
+++ src/components/SobreMi.jsx
@@ -17,7 +17,7 @@ const SobreMi = () => {
               <p>Con experiencia en desarrollo sólido en <b>.NET (C#)</b>, <b>React</b>, <b>Html</b>, <b>Css</b>, <b>jQuery</b>, <b>JavaScript</b>, manejo de versiones de <b>Git</b>, manejo estructura de base de datos <br/> <b>Sql Server</b> y <b>Oracle</b>.</p>
               <p>Mi objetivo es seguir creciendo profesionalmente, aprovechando cada oportunidad para expandir mis habilidades y conocimientos, y contribuir de manera significativa al desarrollo de proyectos tanto a soluciones que impacten positivamente en la sociedad.</p></div>
             <div className='contain-redes'>
-              <a href="https://www.linkedin.com/in/luis-velasco-24b668199" target='_blank'><div className='redes'><i className='bx bxl-linkedin'></i></div></a>
+              <a href="https://www.linkedin.com/in/velascol" target='_blank'><div className='redes'><i className='bx bxl-linkedin'></i></div></a>
               <a href="https://github.com/LV27VELASCO" target='_blank'><div className='redes'><i className='bx bxl-github'></i></div></a>
               <a href="https://www.instagram.com/velasco.27/" target='_blank'><div className='redes'><i className='bx bxl-instagram'></i></div></a>
             </div>
@@ -32,7 +32,7 @@ const SobreMi = () => {
               </a>
               <div className="button-container-2">
                 <span className="hv"><i className='bx bxs-download'></i></span>
-                <a href={curriculum} download='Luis Velasco CV' ><button id='descarga' type="button" name="Hover">DESCARGA CV</button></a>
+                <a href={curriculum} download='Luis_Velasco_CV' ><button id='descarga' type="button" name="Hover">DESCARGA CV</button></a>
               </div>
             </div>
           </div>
